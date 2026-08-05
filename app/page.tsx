fa013811cdbd259979cbb3c027fd3db34cec7e5d
Fiore <fiorellasanchezestevez@gmail.com>
2026-08-04 21:11:43 -0300

    Primera versión del proyecto

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>Mi primera página con Next.js</h1>
+      <h1>Estás observando a Ángel Lux</h1>
       <p>El proyecto está funcionando correctamente.</p>
     </main>
   );
