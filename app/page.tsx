6e6b011dc6a0165f01c6888d84704abc773cb57f
Fiore <fiorellasanchezestevez@gmail.com>
2026-08-04 21:01:42 -0300

    Primer commit del proyecto

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Mi primera página con Next.js</h1>
+      <p>El proyecto está funcionando correctamente.</p>
+    </main>
+  );
+}
